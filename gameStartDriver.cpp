91e65e5879a62cf15179c09bd27b02e025adad97
CJulson <130244611+CJulson@users.noreply.github.com>
2023-04-18 17:30:24 -0600

    Add files via upload

--- gameStartDriver.cpp
+++ gameStartDriver.cpp
@@ -19,13 +19,11 @@ int main() {
     Game testGame = Game();
     Party testParty = Party(testGame);
     baseAction testAction = baseAction(testGame, testParty);
-
     testParty.merchantMenu();
 
 
     //Create Seed For Random Numbers
     srand(time(NULL));  // SEEDS WITH RANDOM TIME
-
 
     //Initialize New Game Map & Set Player and Exit Positions
     Map gameMap;
@@ -48,7 +46,6 @@ int main() {
         }
     }
 
-
     //Set Room Locations 
     int counter2 = 0;
     while (counter2 < 5) 
@@ -68,17 +65,16 @@ int main() {
 
 
 
-
     // MAIN GAME LOOP 
     while(testGame.lose == false && testGame.win == false) 
     {
+        
         // Declare Variables (INTERNAL TO LOOP)
         int selection;
         char direction = 'x';
         bool selection_validity = true;
 
-
-        // Display Party Status, Map, & Recieve Input
+        // Display Party Status & Map
         testParty.printStatus();
         gameMap.displayMap();
 
@@ -191,19 +187,8 @@ int main() {
 
         else
         {
-            cout << "Invalid Input. Please Enter A Number 1-5" << endl;
+            cout << "Invalid Input. Please Enter A Valid Number" << endl;
             selection_validity = false;
-        }
-
-
-
-
-        // Continue If Selection Is Valid
-        if (selection_validity == true)
-        {
-            // AFTER BASE ACTION CODE HERE
-
-            
         }
 
         } // END OF FREE SPACE IF Statement
@@ -213,32 +198,209 @@ int main() {
 
 
         // NPC Space Actions
-        else if(NPCLocation = true )
+        else if(NPCLocation == true )
         {
+            //Receive Input
             cout << "Select one:" << endl;
             cout << "1. Move" << endl;
             cout << "2. Speak to NPC" << endl;
             cout << "3. Give Up" << endl;
             cin >> selection;
-        }
 
+
+            
+            if(selection == 1) // MOVEMENT
+            {
+                // Loop Until Valid Input Recieved 
+                while(direction == 'x')
+                {
+                    cout << "Where would you like to move?" << endl;
+                    cout << "w (up), a (left), s (down), or d (right)" << endl;
+                    cin >> direction;
+
+                    if(direction == 'w')
+                    {
+                        gameMap.move('w'); // Move Up
+                        if (explored != true)
+                        {
+                            // INCREASE SORECERS ANGER ONLY IF ALIVE
+                        }
+                        break;
+                    }
+
+                    else if(direction == 'a')
+                    {
+                        gameMap.move('a'); // Move Left
+                        if (explored != true)
+                        {
+                            // INCREASE SORECERS ANGER ONLY IF ALIVE
+                        }
+                        break;
+                    }
+
+                    else if(direction == 's')
+                    {
+                        gameMap.move('s'); // Move Down
+                        if (explored != true) // Increase Anger
+                        {
+                            // INCREASE SORECERS ANGER ONLY IF ALIVE
+                        }
+                        break;
+                    }
+
+                    else if(direction == 'd')
+                    {
+                        gameMap.move('d'); // Move Right
+                        if (explored != true)
+                        {
+                            // INCREASE SORECERS ANGER ONLY IF ALIVE
+                        }
+                        break; 
+                    }
+
+                    else // Loop through again if invalid input
+                    {
+                        cout << "Invalid Input. Please Enter A Valid Character" << endl;
+                        direction = 'x';
+                    }
+                }
+            }
+
+
+            
+            else if(selection == 2) // NPC SPEAK
+            {
+                testAction.speak();
+            }
+
+
+            else if(selection == 3) // Give Up
+            {
+                testAction.giveUp();
+            }
+
+            else
+            {
+                cout << "Invalid Input. Please Enter A Valid Number" << endl;
+                selection_validity = false;
+            }
+
+        } // END OF NPC ACTION SELECTION
 
 
 
 
 
         // Room Space Actions
-        else if(roomLocation = true )
+        if(roomLocation == true )
         {
             cout << "Select one:" << endl;
             cout << "1. Move" << endl;
             cout << "2. Open Door" << endl;
             cout << "3. Give Up" << endl;
             cin >> selection;
-        }
+
+
+            if(selection == 1)  // Movement
+            {
+                // Loop Until Valid Input Recieved 
+                while(direction == 'x')
+                {
+                    cout << "Where would you like to move?" << endl;
+                    cout << "w (up), a (left), s (down), or d (right)" << endl;
+                    cin >> direction;
+
+                    if(direction == 'w')
+                    {
+                        gameMap.move('w'); // Move Up
+                        if (explored != true)
+                        {
+                            // INCREASE SORECERS ANGER ONLY IF ALIVE
+                        }
+                        break;
+                    }
+
+                    else if(direction == 'a')
+                    {
+                        gameMap.move('a'); // Move Left
+                        if (explored != true)
+                        {
+                            // INCREASE SORECERS ANGER ONLY IF ALIVE
+                        }
+                        break;
+                    }
+
+                    else if(direction == 's')
+                    {
+                        gameMap.move('s'); // Move Down
+                        if (explored != true) // Increase Anger
+                        {
+                            // INCREASE SORECERS ANGER ONLY IF ALIVE
+                        }
+                        break;
+                    }
+
+                    else if(direction == 'd')
+                    {
+                        gameMap.move('d'); // Move Right
+                        if (explored != true)
+                        {
+                            // INCREASE SORECERS ANGER ONLY IF ALIVE
+                        }
+                        break; 
+                    }
+
+                    else // Loop through again if invalid input
+                    {
+                        cout << "Invalid Input. Please Enter A Valid Character" << endl;
+                        direction = 'x';
+                    }
+                }
+            
+            }
+
+
+            
+            else if(selection == 2) // Open Door
+            {
+                testAction.doorOpen();
+            }
+
+
+            else if(selection == 3) // Give Up
+            {
+                testAction.giveUp();
+            }
+
+
+            else
+            {
+                cout << "Invalid Input. Please Enter A Valid Number" << endl;
+                selection_validity = false;
+            }
+
+
+        } // End of Room Space Actions
+        
+        
+        
+        
+        
         // CHECK CONDITIONS THAT COULD POTENTIALLY END GAME (PARTY DEAD, SORCERES ANGER, ECT...)
         // ALSO CHECK FOR PARTY MEMBER CONDITIONS
-        testGame.checkLose();
+
+        //Check if party members alive and updates names/game status
+        int sum = 0;
+        for(int i =0; i < 5; i++) {
+            sum += testParty.members[i].checkAlive();
+        }
+        if(testParty.checkPartyLive() == false) {
+            testGame.setLose();
+        }
+
+        testGame.checkLose();//check win/lose conditions
         testGame.checkWin();
+
+        cout << "End Loop Lose: " << testGame.lose << endl;
     }
 }
